eec1c4f9684fe7767334da3ada3acd967bb2e495
ana1155 <anastasiia.solop@softermii.com>
2021-02-21 12:20:41 +0200

    add paddle payments

--- src/pages/owner/my-restaurant.tsx
+++ src/pages/owner/my-restaurant.tsx
@@ -1,10 +1,12 @@
-import { gql, useQuery } from "@apollo/client";
+import { gql, useMutation, useQuery } from '@apollo/client';
 import React from "react";
 import { Helmet } from "react-helmet-async";
 import { Link, useParams } from "react-router-dom";
 import { myRestaurant, myRestaurantVariables } from "../../api-types/myRestaurant";
 import { DISH_FRAGMENT, RESTAURANT_FRAGMENT } from '../../fragments';
 import { Dish } from '../../components/dish';
+import { useMe } from '../../hooks/useMe';
+import { createPayment, createPaymentVariables } from '../../api-types/createPayment';
 
 export const MY_RESTAURANT_QUERY = gql`
     query myRestaurant($input: MyRestaurantInput!) {
@@ -23,6 +25,15 @@ export const MY_RESTAURANT_QUERY = gql`
     ${DISH_FRAGMENT}
 `;
 
+const CREATE_PAYMENT_MUTATION = gql`
+    mutation createPayment($input: CreatePaymentInput!) {
+        createPayment(input: $input) {
+            ok
+            error
+        }
+    }
+`;
+
 interface IParams {
   id: string;
 }
@@ -39,13 +50,48 @@ export const MyRestaurant = () => {
       },
     }
   );
+  const onCompleted = (data: createPayment) => {
+    if (data.createPayment.ok) {
+      alert("Your restaurant is being promoted!");
+    }
+  };
+  const [createPaymentMutation, { loading }] = useMutation<
+    createPayment,
+    createPaymentVariables
+    >(CREATE_PAYMENT_MUTATION, {
+    onCompleted,
+  });
+  const { data: userData } = useMe();
+  const triggerPaddle = () => {
+    if (userData?.me.email) {
+      // @ts-ignore
+      window.Paddle.Setup({ vendor: 666 });
+      // @ts-ignore
+      window.Paddle.Checkout.open({
+        product: 666,
+        email: userData.me.email,
+        successCallback: (data: any) => {
+          createPaymentMutation({
+            variables: {
+              input: {
+                transactionId: data.checkout.id,
+                restaurantId: +id,
+              },
+            },
+          });
+        },
+      });
+    }
+  };
   return (
     <div>
       <Helmet>
         <title>
           {data?.myRestaurant.restaurant?.name || "Loading..."} | Uber Eats Clone
         </title>
+        <script src="https://cdn.paddle.com/paddle/paddle.js"/>
       </Helmet>
+      <div className="checkout-container"/>
       <div
         className="bg-gray-700 py-28 bg-center bg-cover"
         style={{
@@ -62,9 +108,12 @@ export const MyRestaurant = () => {
         >
           Add Dish &rarr;
         </Link>
-        <Link to={``} className=" text-white bg-lime-700 py-3 px-10">
+        <span
+          onClick={triggerPaddle}
+          className="cursor-pointer text-white bg-lime-700 py-3 px-10"
+        >
           Buy Promotion &rarr;
-        </Link>
+        </span>
         <div className="mt-10">
           {data?.myRestaurant.restaurant?.menu.length === 0 ? (
             <h4 className="text-xl mb-5">Please upload a dish!</h4>
@@ -72,6 +121,7 @@ export const MyRestaurant = () => {
             <div className="grid mt-16 md:grid-cols-3 gap-x-5 gap-y-10">
               {data?.myRestaurant.restaurant?.menu.map((dish) => (
                 <Dish
+                  key={dish.id}
                   name={dish.name}
                   description={dish.description}
                   price={dish.price}
