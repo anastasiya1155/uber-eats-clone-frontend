45ccca155dca085c08729daa7f531335aa47a206
ana1155 <anastasiia.solop@softermii.com>
2021-02-21 12:14:58 +0200

    add dish

--- src/fragments.ts
+++ src/fragments.ts
@@ -23,3 +23,20 @@ export const CATEGORY_FRAGMENT = gql`
     }
 `;
 
+export const DISH_FRAGMENT = gql`
+    fragment DishParts on Dish {
+        id
+        name
+        price
+        photo
+        description
+        options {
+            name
+            extra
+            choices {
+                name
+                extra
+            }
+        }
+    }
+`;
---
+++ src/pages/owner/my-restaurant.tsx
@@ -0,0 +1,86 @@
+import { gql, useQuery } from "@apollo/client";
+import React from "react";
+import { Helmet } from "react-helmet-async";
+import { Link, useParams } from "react-router-dom";
+import { myRestaurant, myRestaurantVariables } from "../../api-types/myRestaurant";
+import { DISH_FRAGMENT, RESTAURANT_FRAGMENT } from '../../fragments';
+import { Dish } from '../../components/dish';
+
+export const MY_RESTAURANT_QUERY = gql`
+    query myRestaurant($input: MyRestaurantInput!) {
+        myRestaurant(input: $input) {
+            ok
+            error
+            restaurant {
+                ...RestaurantParts
+                menu {
+                    ...DishParts
+                }
+            }
+        }
+    }
+    ${RESTAURANT_FRAGMENT}
+    ${DISH_FRAGMENT}
+`;
+
+interface IParams {
+  id: string;
+}
+
+export const MyRestaurant = () => {
+  const { id } = useParams<IParams>();
+  const { data } = useQuery<myRestaurant, myRestaurantVariables>(
+    MY_RESTAURANT_QUERY,
+    {
+      variables: {
+        input: {
+          id: +id,
+        },
+      },
+    }
+  );
+  return (
+    <div>
+      <Helmet>
+        <title>
+          {data?.myRestaurant.restaurant?.name || "Loading..."} | Uber Eats Clone
+        </title>
+      </Helmet>
+      <div
+        className="bg-gray-700 py-28 bg-center bg-cover"
+        style={{
+          backgroundImage: `url(${data?.myRestaurant.restaurant?.coverImg})`,
+        }}
+      />
+      <div className="container mt-10">
+        <h2 className="text-4xl font-medium mb-10">
+          {data?.myRestaurant.restaurant?.name || "Loading..."}
+        </h2>
+        <Link
+          to={`/restaurants/${id}/add-dish`}
+          className=" mr-8 text-white bg-gray-800 py-3 px-10"
+        >
+          Add Dish &rarr;
+        </Link>
+        <Link to={``} className=" text-white bg-lime-700 py-3 px-10">
+          Buy Promotion &rarr;
+        </Link>
+        <div className="mt-10">
+          {data?.myRestaurant.restaurant?.menu.length === 0 ? (
+            <h4 className="text-xl mb-5">Please upload a dish!</h4>
+          ) : (
+            <div className="grid mt-16 md:grid-cols-3 gap-x-5 gap-y-10">
+              {data?.myRestaurant.restaurant?.menu.map((dish) => (
+                <Dish
+                  name={dish.name}
+                  description={dish.description}
+                  price={dish.price}
+                />
+              ))}
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
