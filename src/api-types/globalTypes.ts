588903440a4c94db72418cbc29f2e40ffefd3ba9
ana1155 <anastasiia.solop@softermii.com>
2021-02-21 12:47:34 +0200

    orders

--- src/api-types/globalTypes.ts
+++ src/api-types/globalTypes.ts
@@ -7,6 +7,14 @@
 // START Enums and Input Objects
 //==============================================================
 
+export enum OrderStatus {
+  Cooked = "Cooked",
+  Cooking = "Cooking",
+  Delivered = "Delivered",
+  Pending = "Pending",
+  PickedUp = "PickedUp",
+}
+
 export enum UserRole {
   Client = "Client",
   Delivery = "Delivery",
@@ -32,6 +40,16 @@ export interface CreateDishInput {
   restaurantId: number;
 }
 
+export interface CreateOrderInput {
+  restaurantId: number;
+  items: CreateOrderItemInput[];
+}
+
+export interface CreateOrderItemInput {
+  dishId: number;
+  options?: OrderItemOptionInputType[] | null;
+}
+
 export interface CreatePaymentInput {
   transactionId: string;
   restaurantId: number;
@@ -55,9 +73,18 @@ export interface DishOptionInputType {
   extra?: number | null;
 }
 
+export interface EditOrderInput {
+  id: number;
+  status: OrderStatus;
+}
+
 export interface EditProfileInput {
   email?: string | null;
   password?: string | null;
+}
+
+export interface GetOrderInput {
+  id: number;
 }
 
 export interface LoginInput {
@@ -66,6 +93,15 @@ export interface LoginInput {
 }
 
 export interface MyRestaurantInput {
+  id: number;
+}
+
+export interface OrderItemOptionInputType {
+  name: string;
+  choice?: string | null;
+}
+
+export interface OrderUpdatesInput {
   id: number;
 }
 
